4a4617ae0d675d24fad4ef8e137b3afd1ccaa33e
Đoàn Thị Yến <yendt@htpgroup.com.vn>
2023-07-17 09:55:02 +0700

    add header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'light-coffee': '#C89F94'
+      }
+    },
   },
   plugins: [],
 }
