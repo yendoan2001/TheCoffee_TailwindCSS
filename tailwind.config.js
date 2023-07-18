8f62e066654a762ddcd69b2cdb0016f018f0a68e
Đoàn Thị Yến <yendt@htpgroup.com.vn>
2023-07-18 13:47:12 +0700

    add more products

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,10 +18,15 @@ module.exports = {
                 slideDown: {
                     '0%': {transform: 'translateY(-100%)'},
                     '100%': {transform: 'translateY(0)'},
+                },
+                fadeIn: {
+                    '0%': {opacity: 0},
+                    '100%': {opacity: 1}
                 }
             },
             animation: {
                 slideDown: 'slideDown .4s ease-in-out',
+                fadeIn: 'fadeIn .4s ease-in-out',
             },
             backgroundImage: {
                 'slider': "url('img/slider.jpg')",
